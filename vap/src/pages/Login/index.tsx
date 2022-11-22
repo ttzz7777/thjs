5179802d9d4546054572ebe0aae5ffbc35299194
tianhao <1282533818@qq.com>
2022-11-23 02:15:50 +0800

    2022-11-23

--- vap/src/pages/Login/index.tsx
+++ vap/src/pages/Login/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
-// import styles from './index.module.less'
+import styles from './index.module.less'
 
 const Login: React.FC<any> = () => {
-  return <div>th3</div>
+  console.log(Function instanceof Object)
+  console.log(Array instanceof Object)
+  console.log(Object instanceof Object)
+  console.log(Number instanceof Object)
+  console.log(String instanceof Object)
+  // @ts-ignore
+  console.log(true instanceof Object)
+  // @ts-ignore
+  console.log(undefined instanceof Object)
+  // @ts-ignore
+  console.log(null instanceof Object)
+
+  return <div>123</div>
 }
 
 export default Login
