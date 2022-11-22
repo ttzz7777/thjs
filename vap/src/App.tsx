f70bacd3826591295b2d8a84a4ce6b02aec4a6dd
tianhao <1282533818@qq.com>
2022-11-22 16:08:10 +0800

    2022-11-22

--- vap/src/App.tsx
+++ vap/src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import Login from './pages/Login';
 
 function App() {
-  return <div className="App">React App</div>
+  return <Login />;
 }
-
-export default App
+export default App;
---
+++ vap/src/pages/Login/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+// import styles from './index.module.less'
+
+const Login: React.FC<any> = () => {
+  return <div>th3</div>
+}
+
+export default Login
