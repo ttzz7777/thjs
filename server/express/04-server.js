47a8ec2a30e01ce5c5826ff282c801ec2b355914
tianhao <1282533818@qq.com>
2023-03-06 00:35:59 +0800

    2023-03-06

--- server/express/04-server.js
+++ server/express/04-server.js
@@ -6,6 +6,14 @@ const app = express()
     // extended：ture   方法内部使用第三方模块qs 来处理请求参数的格式
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.all('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    res.header('Access-Control-Allow-Methods', '*')
+    res.header('Content-Type', 'application/json;charset=utf-8')
+    next()
+})
+
 app.post('/add', (req, res) => {
     res.send(req.body)
 })
